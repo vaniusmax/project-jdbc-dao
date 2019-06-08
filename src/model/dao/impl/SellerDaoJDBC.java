abdebf033956f0afb80673e3606ee3c828a1da73
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-06-08 15:19:15 -0300

    Criando um DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,38 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Seller findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
